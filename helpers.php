3daa8724d7af7309fbbc42a5e67906123946bdce
Alwin Pacheco <kesler.alwin@gmail.com>
2019-03-05 10:02:22 -0400

    Update helpers.php
    Kint-php manejará este método

--- helpers.php
+++ helpers.php
@@ -500,20 +500,6 @@ if ( ! function_exists('data_get'))
 	}
 }
 
-if ( ! function_exists('dd'))
-{
-	/**
-	 * Dump the passed variables and end the script.
-	 *
-	 * @param  mixed
-	 * @return void
-	 */
-	function dd()
-	{
-		array_map(function($x) { var_dump($x); }, func_get_args()); die;
-	}
-}
-
 if ( ! function_exists('e'))
 {
 	/**
